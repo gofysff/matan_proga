fdbaa5914e5bd96e4e8ee903bc9f40c3102b5e56
gofysf <toniush75@gmail.com>
2023-05-14 12:04:47 +0300

    final commit

--- individual_homework/main.py
+++ individual_homework/main.py
@@ -2,7 +2,7 @@ import sympy as sp
 
 coeficients_n = {
     2: [-0.577340, 0.577360],
-    3: [-0.707207, 0, -0.707207],
+    3: [-0.707207, 0, 0.707207],
     4: [-0.794653, -0.187591, 0.187591, 0.794653],
     5: [-0.832497, -0.374542, 0, 0.374542, 0.832497],
     6: [-0.866246, -0.422518, -0.266634, 0.266634, 0.422518, 0.866248],
@@ -11,7 +11,7 @@ coeficients_n = {
 }
 
 
-def chebishev(func, a: float, b: float, n: int) -> float:
+def chebishev(func, a: float, b: float, n=9) -> float:
     """
     :param func: function to integrate
     :param a: lower limit
@@ -23,26 +23,44 @@ def chebishev(func, a: float, b: float, n: int) -> float:
     # Calculate the coeficients
     coeficients = coeficients_n[n]
 
-    # coef for all points
-    t_coef = (b+a)/2+(b-a)/2
+    # coef plus for all points
+    t_coef1 = (b+a)/2
+
+    # coef multipl for all points
+    t_coef2 = (b-a)/2
 
     # calculate points
-    points_t = [t_coef*x for x in coeficients]
+    points_t = [t_coef1+t_coef2*x for x in coeficients]
 
     # general coef
     general_coef = (b-a)/n
     return general_coef*sum([func.evalf(subs={x: points_t[i]}) for i in range(n)])
 
 
+def print_results(chebishev, x, f, a, b):
+
+    print(f'Функция {f} c пределами {a}:{b}')
+    # print sympy integral from a to b
+    print('Посчитано при помощи формулы Ньютона-Лейбница')
+    print(sp.integrate(f, (x)).evalf(
+        subs={x: b}) - sp.integrate(f, (x)).evalf(subs={x: a}), end="\n\n")
+    print('Посчитано при помощи формул Чебышева')
+
+    for i in range(2, 7+1):
+        print(f'Результат при n = {i} {chebishev(f, a, b, n =i)}')
+    print(f'Результат при n = 9 {chebishev(f, a, b)}')
+
+
 if __name__ == "__main__":
     x = sp.symbols('x')
 
     f = sp.sin(x) ** 4
-    print(type(f))
     a = 0
     b = 2
-    n = 7
-    print(chebishev(f, a, b, n))
-    # print sympy integral from a to b
-    print(sp.integrate(f, (x)).evalf(
-        subs={x: b}) - sp.integrate(f, (x)).evalf(subs={x: a}))
+    print_results(chebishev, x, f, a, b)
+    print("\n\n")
+
+    f = sp.sqrt(2*x-1)
+    a = 5
+    b = 13
+    print_results(chebishev, x, f, a, b)
